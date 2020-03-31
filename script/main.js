0b0f33c7226a6535824cc5c62f2bcbac7d786b38
Duc Thang Vu <vuducthang91@gmail.com>
2020-03-31 14:00:46 +0200

    first commit

---
+++ script/main.js
@@ -0,0 +1,7 @@
+/* 
+ A script accepting multiple strings by user, adding them to an existing list of strings, and printing all element of the list alphabetically 
+  
+ */
+
+
+console.log('main.js is working')
