30596f032cbe6ba65d8cda10593ee00accbb84e3
Duc Thang Vu <vuducthang91@gmail.com>
2020-03-31 18:36:03 +0200

    fixing

--- script/main.js
+++ script/main.js
@@ -14,7 +14,7 @@ var message = document.getElementById('admin-msg')
 
 // FUNCTIONS
 function formatting_surnames(text) {
-    // A function accepting a string, and returning the same string, the first letter being capitalized and the others unmodified. Return -1 if: input type is number or empty.
+    // A function accepting a string, and returning the same string, the first letter being capitalized and the others unmodified. Return -1 if: input is of type number, or empty.
 
     if ((text) && isNaN(text)) {
         return text.charAt(0).toUpperCase() + text.slice(1)
